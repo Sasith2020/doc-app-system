8ef26aeec97376ef84032ceb6a2637bc38c6dce3
IT21039140 <it21039140@my.sliit.lk>
2026-01-05 23:26:23 +0530

    top nav arrangement changes

--- src/components/dashboard/TopNav.tsx
+++ src/components/dashboard/TopNav.tsx
@@ -5,13 +5,13 @@ type TopNavProps = {
 function TopNav({ onLogout }: TopNavProps) {
   return (
     <header className="top-nav">
+      <span className="user-chip">UID-39821</span>
+      <span className="user-chip">Jordan Lee</span>
+      <span className="pill">Level 2</span>
+      <span className="user-chip">DIV-04</span>
       <button type="button" className="link-btn" onClick={onLogout}>
         Log out
       </button>
-      <span className="pill">Level 2</span>
-      <span className="user-chip">Jordan Lee</span>
-      <span className="user-chip">DIV-04</span>
-      <span className="user-chip">UID-39821</span>
     </header>
   )
 }
